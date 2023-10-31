d53a2e34efe3a3ccef7ba90df4f81e953e9ac854
gs-window-vbox <gltxdy@outlook.com>
2023-10-31 10:56:28 +0000

    ok

--- panel/Excel.js
+++ panel/Excel.js
@@ -18,10 +18,8 @@ class Excel {
 	 */
 	projectPath
 	/**
-	 * 自定义类型信息
-	 * @type {{[x:string]:{type:string,getValue:Function}}}
+	 * 面板上的label 可显示一段文字
 	 */
-	allTypeData
 	stateLabel
 	/**
 	 * 在面板上显示文字
@@ -33,51 +31,6 @@ class Excel {
 	constructor(stateLabel) {
 		this.stateLabel = stateLabel
 		this.projectPath = Editor.Project.path
-		this.allTypeData = {
-			'number': {
-				type: 'number',
-				getValue: this.toNumber
-			},
-			'numbers': {
-				type: 'Array<number>',
-				getValue: this.toNumber
-			},
-
-			'float': {
-				type: 'number',
-				getValue: this.toNumber
-			},
-			'list<float>': {
-				type: 'Array<number>',
-				getValue: this.toListNumber
-			},
-
-			'str': {
-				type: 'string',
-				getValue: this.toString
-			},
-			'string': {
-				type: 'string',
-				getValue: this.toString
-			},
-			'list<string>': {
-				type: 'Array<string>',
-				getValue: this.toListString
-			},
-
-			'boo': {
-				type: 'boolean',
-				getValue: this.toBoolean
-			},
-			'bool': {
-				type: 'boolean',
-				getValue: this.toBoolean
-			},
-			'boolean': {
-				type: 'boolean',
-				getValue: this.toBoolean
-			}
-		}
 	}
 	/**
 	 * @param {string} str 
@@ -152,22 +105,79 @@ class Excel {
 			return
 		}
 		this.showState('start generate')
-		let saveRoot = this.projectPath + '/assets/data'
-		let xlsxRoot = this.projectPath + '/excel'
 
-		let xlsxPaths = this.readFileList(fs, xlsxRoot)
+		let xlsxPaths = this.readFileList(this.projectPath + '/excel')
 		let xlsxs = this.parseXlsx(xlsxPaths)
 
-		this.save_ConfigTypeDefind_ts(xlsxs, saveRoot)
+		this.save_ConfigTypeDefind_ts(xlsxs)
 
-		this.save_dataManager_ts(xlsxs, saveRoot)
+		this.save_dataManager_ts(xlsxs)
 
-		this.save_datas_json(xlsxs, saveRoot)
+		this.save_datas_json(xlsxs)
 
 		//刷新资源管理器
 		Editor.assetdb.refresh('db://assets/data/')
 
 		this.showState('generate complete')
+	}
+	/**
+	 * 获取属性的类型
+	 * @param {{name:string,data:Array<Array<string|number>>}} sheet 
+	 * @param {number} index 
+	 */
+	getType(sheet, index) {
+		let len = Math.min(12, sheet.data.length)
+		let isStr = false
+		for (let i = 2; i < len; i++) {
+			const data = sheet.data[i][index]
+			if (typeof data == 'string') {
+				isStr = true
+			}
+		}
+		let isArray = false
+		for (let i = 2; i < len; i++) {
+			const data = sheet.data[i][index] + ''
+			if (data.includes('|')) {
+				isArray = true
+			}
+		}
+		if (isArray) {
+			// 假设全是数字 循环找只要有一个不是数字就字符串
+			isStr = false
+			for (let i = 2; i < len; i++) {
+				const data = sheet.data[i][index] + ''
+				if (data.includes('|')) {
+					let array = data.split('|')
+					for (let k = 0; k < array.length; k++) {
+						if (isNaN(Number(array[k]))) {
+							isStr = true
+						}
+					}
+				}
+			}
+			if (isStr) {
+				return 'Array<string>'
+			} else {
+				return 'Array<number>'
+			}
+		} else {
+			if (isStr) {
+				return 'string'
+			} else {
+				return 'number'
+			}
+		}
+	}
+	/**
+	 * 把字符串保存为一个文件
+	 * @private
+	 * @param {string} str 
+	 * @param {string} file 
+	 */
+	saveFile(str, file) {
+		let baseurl = this.projectPath + '/assets/data'
+		let url = path.join(baseurl, file)
+		fs.writeFileSync(url, str, { encoding: 'utf-8' })
 	}
 	/**
 	 * 解析所有excel文件
@@ -195,12 +205,11 @@ class Excel {
 	/**
 	 * 递归读取目录下所有文件
 	 * @private
-	 * @param {*} fs 导入的fs模块
 	 * @param {string} dir 目录
 	 * @param {string[]} fileList 文件列表
 	 * @returns 文件列表
 	 */
-	readFileList(fs, dir, fileList = []) {
+	readFileList(dir, fileList = []) {
 		//读取一个目录下的所有文件/目录
 		let files = fs.readdirSync(dir)
 		files.forEach(file => {
@@ -210,7 +219,7 @@ class Excel {
 			//如果这是一个目录
 			if (stats.isDirectory()) {
 				//读取目录里的文件
-				this.readFileList(fs, path.join(dir, file), fileList)
+				this.readFileList(path.join(dir, file), fileList)
 			} else {
 				//这是一个文件,文件路径加入列表
 				fileList.push(fullPath)
@@ -225,63 +234,57 @@ class Excel {
 	 * @returns 
 	 */
 	save_ConfigTypeDefind_ts(xlsxs, saveRoot) {
-		let str = `class Data {
-	protected _data: Array<any>
-	/** ID */
-	get ID(): number { return this._data[0] }
-}\n`
+		let str = ''
+		str += 'class Data {\n'
+		str += '    protected _data: Array<any>\n'
+		str += '    /** ID */\n'
+		str += '    get ID(): number { return this._data[0] }\n'
+		str += '}\n'
+
 		for (const xlsxName in xlsxs) {
 			let xlsx = xlsxs[xlsxName]
 			let sheet1 = xlsx[0]
-			if (sheet1.data.length < 4) {
-				Editor.log(`表 ${xlsxName} 行数小于3行,跳过`)
+			if (sheet1.data.length < 3) {
+				this.log(`表 ${xlsxName} 行数小于3行, 跳过`)
 				return
 			}
-			/** @type {string[]} */
-			let line1 = sheet1.data[0]
-			/** @type {string[]} */
-			let line2 = sheet1.data[1]
-			/** @type {string[]} */
-			let line3 = sheet1.data[2]
+			/** 
+			 * 第一行 中文名
+			 * @type {string[]}
+			 */
+			let Chinese_name_list = sheet1.data[0]
+			/** 
+			 * 第二行 英文名
+			 * @type {string[]}
+			 */
+			let English_name_list = sheet1.data[1]
+
+			// 保存类型到sheet中 第一个是id number类型
+			sheet1.type = ['number']
 
 			//拼凑ts文件的字符串
-			str += `export class ${xlsxName}Data extends Data{\n`
+			str += `export class ${xlsxName}Data extends Data {\n`
 			//拼凑每一条属性
 			let delect = 0
-			for (let i = 1; i < line3.length; i++) {
-				/** 变量中文描述 */
-				let cnName = line1[i]
-				/** 变量英文名字 */
-				let enName = line2[i]
-				/** 变量类型 */
-				let type = line3[i]
-				if (type === undefined) {
-					delect++
-					continue
-				}
-				/** @type {string} 把字符串转换为小写 */
-				let lowType = type.toLowerCase()
-				//加上注释
-				str += `    /** ${cnName} */\n`
-				//变量名字
+			for (let i = 1; i < English_name_list.length; i++) {
+				let Chinese_name = Chinese_name_list[i]
+				let English_name = English_name_list[i]
+				//注释
+				str += `    /** ${Chinese_name} */\n`
+				//变量名
 				//get DiamondReward(): number { return this._data[1] }
-				str += `    get ${enName}(): `
-				//取出自定义的类型信息
-				let typeData = this.allTypeData[lowType]
-				if (typeData) {
-					str += typeData.type
-				} else {
-					str += 'any'
-					this.log('[未知类型]' + type, __filename)
-				}
+				str += `    get ${English_name}(): `
+				//类型
+				let type = this.getType(sheet1, i)
+				sheet1.type.push(type)
+				str += type
 				//换行
 				str += ` { return this._data[${i - delect}] }\n`
 			}
-			str += `}\n`
+			str += `} \n`
 		}
 		//写入文件保存
-		fs.writeFileSync(path.join(saveRoot, "ConfigTypeDefind.ts"),
-			str, { encoding: 'utf-8' })
+		this.saveFile(str, "ConfigTypeDefind.ts")
 	}
 	/**
 	 * @private
@@ -298,29 +301,22 @@ class Excel {
 		for (const xlsxName in xlsxs) {
 			let xlsx = xlsxs[xlsxName]
 			let sheet1 = xlsx[0]
-			if (sheet1.data.length < 4) {
+			if (sheet1.data.length < 3) {
 				return
 			}
-			//id的类型
-			let type = sheet1.data[2][0]
-			let lowType = type.toLowerCase()
-			let idType = this.allTypeData[lowType].type
-			if (!idType) continue
 
-			importContent += `import {${xlsxName}Data} from "./ConfigTypeDefind"\n`
-			defindContent += `export let ${xlsxName}Datas:Array<${xlsxName}Data>\n`
-			defindContent += `export let ${xlsxName}DatasById:{[key in ${idType}]:${xlsxName}Data}\n`
-			funcContent += `${xlsxName}Datas=arrayData(${xlsxName}Data,"${xlsxName}",datas)\n`
-			funcContent += `${xlsxName}DatasById=getsById(${xlsxName}Datas)\n`
+			importContent += `import { ${xlsxName}Data } from "./ConfigTypeDefind"\n`
+			defindContent += `	export let ${xlsxName}Datas: Array<${xlsxName}Data>\n`
+			defindContent += `	export let ${xlsxName}DatasById: { [key in number]: ${xlsxName}Data }\n`
+			funcContent += `		${xlsxName}Datas = arrayData(${xlsxName}Data, "${xlsxName}", datas) \n`
+			funcContent += `		${xlsxName}DatasById = getsById(${xlsxName}Datas) \n`
 		}
 		//在这个三个位置替换字符串
 		clazData = clazData.replace("@@import", importContent)
 		clazData = clazData.replace("@@varDefined", defindContent)
 		clazData = clazData.replace("@@funcContent", funcContent)
 		//保存文件
-
-		fs.writeFileSync(path.join(saveRoot, "DataManager.ts"),
-			clazData, { encoding: 'utf-8' })
+		this.saveFile(clazData, "DataManager.ts")
 	}
 	/**
 	 * @private
@@ -334,36 +330,45 @@ class Excel {
 			json[xlsxName] = {}
 			let xlsx = xlsxs[xlsxName]
 			let sheet1 = xlsx[0]
-			let sheetdata = sheet1.data
-			if (sheetdata.length < 4) {
+			if (sheet1.data.length < 3) {
 				return
 			}
-			let enNames = sheetdata[1]
-			let types = sheetdata[2]
-			//从第4行(下标为3)开始读取数据 生成数据对象
-			a: for (let row = 3; row < sheetdata.length; row++) {
-				let rowdatas = sheetdata[row]
+			let English_name_list = sheet1.data[1]
+			let types = sheet1.type
+			//从第3行(下标为2)开始读取数据 生成数据对象
+			for (let row = 2; row < sheet1.data.length; row++) {
+				let rowdatas = sheet1.data[row]
 				if (rowdatas.length === 0) continue
 				//一行代表一个 data(数据对象) 
 				let data = []
 				//给这个 data(数据对象) 添加表里的属性 
-				for (let i = 0; i < enNames.length; i++) {
+				for (let i = 0; i < English_name_list.length; i++) {
 					let type = types[i]
-					if (type === undefined) continue
 					let value = rowdatas[i]
-					let lowType = type.toLowerCase()
-					let typeData = this.allTypeData[lowType]
-					if (typeData) {
-						data.push(typeData.getValue(value))
+					switch (type) {
+						case 'number':
+							data.push(value)
+							break;
+						case 'string':
+							data.push(`${value}`)
+							break;
+						case 'Array<number>':
+							let array_number = []
+							let value_str = value + ''
+							value_str.split('|').forEach(v => {
+								array_number.push(Number(v))
+							})
+							data.push(array_number)
+							break;
+						case 'Array<string>':
+							data.push(value.split('|'))
+							break;
 					}
 				}
 				json[xlsxName][data[0]] = data
 			}
 		}
-
-		let str = JSON.stringify(json)
-		fs.writeFileSync(path.join(saveRoot, "datas.json")
-			, str, { encoding: 'utf-8' })
+		this.saveFile(JSON.stringify(json), 'datas.json')
 	}
 }
 
